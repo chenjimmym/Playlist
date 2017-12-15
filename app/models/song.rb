ed9f17d61f0dd799eab749d02f81761afafcefa3
Jimmy <chen.jimmym@gmail.com>
2017-12-15 11:51:15 -0800

    all working, need to add authencity

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,6 @@
 class Song < ActiveRecord::Base
     has_many :song_addeds, dependent: :destroy
     has_many :added_users, through: :song_addeds, source: :user
+
+    validates :title, :artist, presence: true, length: {minimum: 2}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ActiveRecord::Base
   has_many :song_addeds, dependent: :destroy
   has_many :added_songs, through: :song_addeds, source: :song
 
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :first_name, :last_name, presence: true, length: {minimum: 2}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+  validates :password, presence:true, length: {minimum: 8}, on: :create
+
   has_secure_password
 end
